6f966f843f0b104aa2da8c6f45e7dc744e517039
Ankit Kumar <78913240+Ankitkumar803@users.noreply.github.com>
2023-07-31 00:46:38 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,7 +33,9 @@ print(client.list_collections())
 @app.post("/qna")
 def assess_diversification(question:Question, company:Company, max_new_token: MAX_NEW_TOKEN ):
     try:
-        query = "what intivative are taken in Rural tourism"    
+        query = question
+        print(question , company , max_new_token)
+        
         db = Chroma(client=client, collection_name= company , embedding_function=instructor_embeddings)
         docs = db.similarity_search(query, k=3 ) # k = 3
         print(docs[0].page_content)
@@ -41,7 +43,7 @@ def assess_diversification(question:Question, company:Company, max_new_token: MA
         context = ""
         for doc in docs:
             context += "\n" + doc.page_content
-        question = question  
+        question = query 
         template = """<|prompt|>Use the following pieces of context to answer the question at the end, don't use information outside the context.
 
         If you don't know the answer, just say that you don't know, don't try to make up an answer. 
